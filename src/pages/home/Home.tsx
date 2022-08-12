cc5c862c1892266b1716f6b875cda7c6bd7d0e1a
theobassan <theobassan@gmail.com>
2022-08-12 00:16:42 -0300

    add decada

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,18 +3,108 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Table, Row, Rows } from 'react-native-table-component';
 
 import { Text } from '@components';
+import { Obra } from '@domain';
+import * as analisys_list_utils from '@utils/data/analisys_list_utils';
 import * as obras from '@utils/data/obra';
 import * as obra_artepublica from '@utils/data/obra_artepublica';
 
+function Test(): JSX.Element[] {
+    const result: JSX.Element[] = [];
+
+    const style = styles();
+
+    for (let index = 1500; index < 2030; index = index + 10) {
+        const obras_decada: Obra[] = analisys_list_utils[`all${index}`];
+
+        if (obras_decada.length > 0) {
+            const tipologias_obras_decada: string[] = obras_decada.map((obra) => obra.Tipologia ?? 'Desconhecida');
+            const naturezas_obras_decada: string[] = obras_decada.map((obra) => obra.Natureza ?? 'Desconhecida');
+            const zonas_obras_decada: string[] = obras_decada.map((obra) => obra.Zona ?? 'Desconhecida');
+
+            const tipologias_obras_decada_total: { nome: string; total: number }[] = tipologias_obras_decada
+                .reduce<{ nome: string; total: number }[]>(function (r, a) {
+                    const r_top = r.find((top) => top.nome === a);
+                    if (!r_top) {
+                        r.push({
+                            nome: a,
+                            total: tipologias_obras_decada.filter((top) => top === a).length,
+                        });
+                    }
+                    return r;
+                }, [])
+                .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+            const naturezas_obras_decada_total: { nome: string; total: number }[] = naturezas_obras_decada
+                .reduce<{ nome: string; total: number }[]>(function (r, a) {
+                    const r_top = r.find((top) => top.nome === a);
+                    if (!r_top) {
+                        r.push({
+                            nome: a,
+                            total: naturezas_obras_decada.filter((top) => top === a).length,
+                        });
+                    }
+                    return r;
+                }, [])
+                .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+            const zonas_obras_decada_total: { nome: string; total: number }[] = zonas_obras_decada
+                .reduce<{ nome: string; total: number }[]>(function (r, a) {
+                    const r_top = r.find((top) => top.nome === a);
+                    if (!r_top) {
+                        r.push({
+                            nome: a,
+                            total: zonas_obras_decada.filter((top) => top === a).length,
+                        });
+                    }
+                    return r;
+                }, [])
+                .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+            result.push(
+                <View key={index}>
+                    <Text>
+                        {index}: {obras_decada.length}
+                    </Text>
+                    <View style={{ height: 24 }} />
+
+                    <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                        <Row data={[<Text>Tipologia</Text>, <Text>Total</Text>]} style={style.head} />
+                        <Rows data={tipologias_obras_decada_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
+                    </Table>
+                    <View style={{ height: 24 }} />
+
+                    <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                        <Row data={[<Text>Natureza</Text>, <Text>Total</Text>]} style={style.head} />
+                        <Rows data={naturezas_obras_decada_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
+                    </Table>
+                    <View style={{ height: 24 }} />
+
+                    <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                        <Row data={[<Text>Zona</Text>, <Text>Total</Text>]} style={style.head} />
+                        <Rows data={zonas_obras_decada_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
+                    </Table>
+                    <View style={{ height: 24 }} />
+                </View>,
+            );
+        }
+    }
+
+    return result;
+}
+
 function Home(): JSX.Element {
+    const style = styles();
+
     const tipologias: string[] = Object.keys(obras).map((key) => obras[key].TipologiaObra ?? 'Desconhecida');
     const tipologias_artepublica: string[] = Object.keys(obra_artepublica).map((key) => obra_artepublica[key].Tipologia ?? 'Desconhecida');
+    const naturezas_artepublica: string[] = Object.keys(obra_artepublica).map((key) => obra_artepublica[key].Natureza ?? 'Desconhecida');
+    const zonas_artepublica: string[] = Object.keys(obra_artepublica).map((key) => obra_artepublica[key].Zona ?? 'Desconhecida');
 
     const all_topologias: string[] = [];
     Array.prototype.push.apply(all_topologias, tipologias);
     Array.prototype.push.apply(all_topologias, tipologias_artepublica);
 
-    const topologias_group: { nome: string; total: number }[] = all_topologias
+    const topologias_group_total: { nome: string; total: number }[] = all_topologias
         .reduce<{ nome: string; total: number }[]>(function (r, a) {
             const r_top = r.find((top) => top.nome === a);
             if (!r_top) {
@@ -27,7 +117,7 @@ function Home(): JSX.Element {
         }, [])
         .sort((a, b) => (a.total < b.total ? 1 : -1));
 
-    const tipologias_artepublica_group: { nome: string; total: number }[] = tipologias_artepublica
+    const tipologias_artepublica_group_total: { nome: string; total: number }[] = tipologias_artepublica
         .reduce<{ nome: string; total: number }[]>(function (r, a) {
             const r_top = r.find((top) => top.nome === a);
             if (!r_top) {
@@ -40,8 +130,34 @@ function Home(): JSX.Element {
         }, [])
         .sort((a, b) => (a.total < b.total ? 1 : -1));
 
+    const naturezas_artepublica_group_total: { nome: string; total: number }[] = naturezas_artepublica
+        .reduce<{ nome: string; total: number }[]>(function (r, a) {
+            const r_top = r.find((top) => top.nome === a);
+            if (!r_top) {
+                r.push({
+                    nome: a,
+                    total: naturezas_artepublica.filter((top) => top === a).length,
+                });
+            }
+            return r;
+        }, [])
+        .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+    const zonas_artepublica_group_total: { nome: string; total: number }[] = zonas_artepublica
+        .reduce<{ nome: string; total: number }[]>(function (r, a) {
+            const r_top = r.find((top) => top.nome === a);
+            if (!r_top) {
+                r.push({
+                    nome: a,
+                    total: zonas_artepublica.filter((top) => top === a).length,
+                });
+            }
+            return r;
+        }, [])
+        .sort((a, b) => (a.total < b.total ? 1 : -1));
+
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={style.container}>
             <ScrollView style={{ width: '100%' }}>
                 <Text>Bem vinda ao inventário de arte pública do Rio de Janeiro</Text>
                 <View style={{ height: 24 }} />
@@ -50,8 +166,8 @@ function Home(): JSX.Element {
                 <View style={{ height: 24 }} />
 
                 <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-                    <Row data={['Tipologia', 'Total']} style={styles.head} textStyle={styles.text} />
-                    <Rows data={topologias_group.map((top) => [top.nome, top.total])} textStyle={styles.text} />
+                    <Row data={[<Text>Tipologia</Text>, <Text>Total</Text>]} style={style.head} />
+                    <Rows data={topologias_group_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
                 </Table>
                 <View style={{ height: 24 }} />
 
@@ -59,23 +175,38 @@ function Home(): JSX.Element {
                 <View style={{ height: 24 }} />
 
                 <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-                    <Row data={['Tipologia', 'Total']} style={styles.head} textStyle={styles.text} />
-                    <Rows data={tipologias_artepublica_group.map((top) => [top.nome, top.total])} textStyle={styles.text} />
+                    <Row data={[<Text>Tipologia</Text>, <Text>Total</Text>]} style={style.head} />
+                    <Rows data={tipologias_artepublica_group_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
                 </Table>
                 <View style={{ height: 24 }} />
+
+                <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                    <Row data={[<Text>Natureza</Text>, <Text>Total</Text>]} style={style.head} />
+                    <Rows data={naturezas_artepublica_group_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
+                </Table>
+                <View style={{ height: 24 }} />
+
+                <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                    <Row data={[<Text>Zona</Text>, <Text>Total</Text>]} style={style.head} />
+                    <Rows data={zonas_artepublica_group_total.map((top) => [<Text>{top.nome}</Text>, <Text>{top.total}</Text>])} />
+                </Table>
+                <View style={{ height: 24 }} />
+
+                {Test()}
             </ScrollView>
         </SafeAreaView>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    head: { height: 40 },
-    text: { margin: 6, color: 'white' },
-});
+function styles() {
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        head: { height: 40 },
+    });
+}
 
 export default Home;
---
+++ src/utils/data/analisys_utils.ts
@@ -0,0 +1,232 @@
+export const isDecade = (date: string | undefined, decade: string): boolean => {
+    if (date != null && date !== '') {
+        try {
+            if (date.split('/').length === 3) {
+                return date.split('/')[2].startsWith(decade);
+            } else if (date.split('/').length === 2) {
+                return date.split('/')[1].startsWith(decade);
+            } else {
+                return date.startsWith(decade);
+            }
+        } catch {
+            console.log(date);
+        }
+    }
+    return false;
+};
+
+export const is2020 = (date?: string): boolean => {
+    return isDecade(date, '202');
+};
+
+export const is2010 = (date?: string): boolean => {
+    return isDecade(date, '201');
+};
+
+export const is2000 = (date?: string): boolean => {
+    return isDecade(date, '200');
+};
+
+export const is1990 = (date?: string): boolean => {
+    return isDecade(date, '199');
+};
+
+export const is1980 = (date?: string): boolean => {
+    return isDecade(date, '198');
+};
+
+export const is1970 = (date?: string): boolean => {
+    return isDecade(date, '197');
+};
+
+export const is1960 = (date?: string): boolean => {
+    return isDecade(date, '196');
+};
+
+export const is1950 = (date?: string): boolean => {
+    return isDecade(date, '195');
+};
+
+export const is1940 = (date?: string): boolean => {
+    return isDecade(date, '194');
+};
+
+export const is1930 = (date?: string): boolean => {
+    return isDecade(date, '193');
+};
+
+export const is1920 = (date?: string): boolean => {
+    return isDecade(date, '192');
+};
+
+export const is1910 = (date?: string): boolean => {
+    return isDecade(date, '191');
+};
+
+export const is1900 = (date?: string): boolean => {
+    return isDecade(date, '190');
+};
+
+export const is1890 = (date?: string): boolean => {
+    return isDecade(date, '189');
+};
+
+export const is1880 = (date?: string): boolean => {
+    return isDecade(date, '188');
+};
+
+export const is1870 = (date?: string): boolean => {
+    return isDecade(date, '187');
+};
+
+export const is1860 = (date?: string): boolean => {
+    return isDecade(date, '186');
+};
+
+export const is1850 = (date?: string): boolean => {
+    return isDecade(date, '185');
+};
+
+export const is1840 = (date?: string): boolean => {
+    return isDecade(date, '184');
+};
+
+export const is1830 = (date?: string): boolean => {
+    return isDecade(date, '183');
+};
+
+export const is1820 = (date?: string): boolean => {
+    return isDecade(date, '182');
+};
+
+export const is1810 = (date?: string): boolean => {
+    return isDecade(date, '181');
+};
+
+export const is1800 = (date?: string): boolean => {
+    return isDecade(date, '180');
+};
+
+export const is1790 = (date?: string): boolean => {
+    return isDecade(date, '179');
+};
+
+export const is1780 = (date?: string): boolean => {
+    return isDecade(date, '178');
+};
+
+export const is1770 = (date?: string): boolean => {
+    return isDecade(date, '177');
+};
+
+export const is1760 = (date?: string): boolean => {
+    return isDecade(date, '176');
+};
+
+export const is1750 = (date?: string): boolean => {
+    return isDecade(date, '175');
+};
+
+export const is1740 = (date?: string): boolean => {
+    return isDecade(date, '174');
+};
+
+export const is1730 = (date?: string): boolean => {
+    return isDecade(date, '173');
+};
+
+export const is1720 = (date?: string): boolean => {
+    return isDecade(date, '172');
+};
+
+export const is1710 = (date?: string): boolean => {
+    return isDecade(date, '171');
+};
+
+export const is1700 = (date?: string): boolean => {
+    return isDecade(date, '170');
+};
+
+export const is1690 = (date?: string): boolean => {
+    return isDecade(date, '169');
+};
+
+export const is1680 = (date?: string): boolean => {
+    return isDecade(date, '168');
+};
+
+export const is1670 = (date?: string): boolean => {
+    return isDecade(date, '167');
+};
+
+export const is1660 = (date?: string): boolean => {
+    return isDecade(date, '166');
+};
+
+export const is1650 = (date?: string): boolean => {
+    return isDecade(date, '165');
+};
+
+export const is1640 = (date?: string): boolean => {
+    return isDecade(date, '164');
+};
+
+export const is1630 = (date?: string): boolean => {
+    return isDecade(date, '163');
+};
+
+export const is1620 = (date?: string): boolean => {
+    return isDecade(date, '162');
+};
+
+export const is1610 = (date?: string): boolean => {
+    return isDecade(date, '161');
+};
+
+export const is1600 = (date?: string): boolean => {
+    return isDecade(date, '160');
+};
+
+export const is1590 = (date?: string): boolean => {
+    return isDecade(date, '159');
+};
+
+export const is1580 = (date?: string): boolean => {
+    return isDecade(date, '158');
+};
+
+export const is1570 = (date?: string): boolean => {
+    return isDecade(date, '157');
+};
+
+export const is1560 = (date?: string): boolean => {
+    return isDecade(date, '156');
+};
+
+export const is1550 = (date?: string): boolean => {
+    return isDecade(date, '155');
+};
+
+export const is1540 = (date?: string): boolean => {
+    return isDecade(date, '154');
+};
+
+export const is1530 = (date?: string): boolean => {
+    return isDecade(date, '153');
+};
+
+export const is1520 = (date?: string): boolean => {
+    return isDecade(date, '152');
+};
+
+export const is1510 = (date?: string): boolean => {
+    return isDecade(date, '151');
+};
+
+export const is1500 = (date?: string): boolean => {
+    return isDecade(date, '150');
+};
+
+export const isNull = (date?: string): boolean => {
+    return date === null || date === ''; // || date.split('/')[1] === null || date.split('/')[1] === '';
+};
